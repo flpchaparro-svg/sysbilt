c48d679726109885f68fc9ad85a50331c22c46cf
flpchaparro-svg <flp.chaparro@gmail.com>
2026-05-12 21:49:17 +1000

    privacy policy

--- src/pages/PrivacyPolicyPage.tsx
+++ src/pages/PrivacyPolicyPage.tsx
@@ -21,7 +21,6 @@ const PrivacyPolicyPage: React.FC<PrivacyPolicyPageProps> = ({ onBack }) => {
         title={SEO_META.privacy.title}
         description={SEO_META.privacy.description}
         canonical={SEO_META.privacy.canonical}
-        robots="noindex"
       />
       <div className="max-w-[1000px] mx-auto px-6 md:px-12 lg:px-20 pt-24 pb-32">
         <div className="mb-16">
